5f5ff7fc46a97a145641cbde8dfba388f3ff9db0
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-04-01 19:59:19 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,6 @@
 # your code goes here
 class Person
   attr_reader :name
-  attr_accessor :bank_account 
+  attr_accessor :bank_account
+  def initialize(name, bank_account = 25, happiness = 8)
 end
