280b0b5618ba0353b651e6f774d794e4523d7252
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-04-01 20:00:06 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,4 +3,13 @@ class Person
   attr_reader :name
   attr_accessor :bank_account
   def initialize(name, bank_account = 25, happiness = 8)
+    @name = name
+    @bank_account = bank_account
+    @happiness = happiness
+  end
+
+
+  def happiness=(number)
+    if happiness
+    end 
 end
